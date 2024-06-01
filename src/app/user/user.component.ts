8c49af54f088dc70a96cf8c6359c061b90363a8f
Vignesh Moorthy <madhanvic@gmail.com>
2024-06-01 22:59:41 +0530

    Signals and Inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,14 +11,7 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-
-  get imagePath() {
-    return `assets/users/${this.selectedUser.avatar}`;
-  }
-
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+  @Input({ required: true  }) name!: string;
+  @Input() avatar!: string;
+  onSelectUser() {}
 }
